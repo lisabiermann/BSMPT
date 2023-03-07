1a29bbf4bddb5f08e7e395f502012127c4802cba
Lisa Biermann <lisa.biermann@kit.edu>
2023-03-07 14:29:34 +0100

    modified prepareData script for CN2HDM

--- sh/prepareData_CN2HDM.py
+++ sh/prepareData_CN2HDM.py
@@ -1,17 +1,13 @@
-# SPDX-FileCopyrightText: 2021 Philipp Basler, Margarete Mühlleitner and Jonas Müller
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
+#!/usr/bin/env python3
 
 import pandas as pd
-import argparse
+import sys
 
 ####### The parameters Type,Lambda1 to Lambda8, tanbeta, m12squared and v_s should have the label of the
 ####### corresponding parameter. With Seperator you have to tell which seperator your data file
 ####### is using (e.g. , \t or space). Your InputFILE will then be saved to OutputFILE.
 
 Seperator='\t'
-InputFILE='../example/CN2HDM_Input.dat'
-OutputFILE='CN2HDM_Ordered.dat'
 Type='yuktype'
 Lambda1='L1'
 Lambda2='L2'
@@ -25,15 +21,17 @@ Lambda8='L8'
 Rem12Sq='re_m12sq'
 mDM='mDM'
 TanBeta='tbeta'
-vs='p_vs'
+vs='vs'
 
 
-def convert(IndexCol):
+def convert(IndexCol, InputFILE, OutputFILE):
 	df=pd.DataFrame()
 	if IndexCol == 'False':
-		df=pd.read_table(InputFILE,index_col=False,sep=Seperator)
+		with open(InputFILE, 'r') as file:
+			df=pd.read_csv(file, index_col=False, sep=Seperator)
 	else:
-		df=pd.read_table(InputFILE,index_col=int(IndexCol),sep=Seperator)
+		with open(InputFILE, 'r') as file:
+			df=pd.read_csv(file, index_col=int(IndexCol), sep=Seperator)
 
 	frontcol=[Type,Lambda1,Lambda2,Lambda3,Lambda4,ReLambda5,ImLambda5,Lambda6,Lambda7,Lambda8,Rem12Sq,mDM,TanBeta,vs]
 
@@ -42,12 +40,8 @@ def convert(IndexCol):
 	df = df.rename(columns={'mDM': 'mDMSq'})
 	df['mDMSq'] = df['mDMSq']**2 # loaded value is just mDM
 
-	df.to_csv(OutputFILE,index=False,sep='\t')
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-i','--indexcol',help='Column which stores the index of your data', default='False')
+	with open(OutputFILE, 'w') as file:
+		df.to_csv(file, index=False, sep='\t')
 
 if __name__ == "__main__":
-	args = parser.parse_args()
-	convert(args.indexcol)
-
+    convert('False', sys.argv[1], sys.argv[2])
